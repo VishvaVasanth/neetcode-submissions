004ee307758cb24bff39126f24beb8a48a3e1225
Vishvavasanth <vishvavasanth33@gmail.com>
2026-08-17 19:30:23 +0530

    Add: jump-game - submission-5

---
+++ Data Structures & Algorithms/jump-game/submission-5.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+
+        maxReach = 0
+
+        for i in range(len(nums)):
+            if i>maxReach:
+                return False
+
+            maxReach = max(maxReach,nums[i]+i)
+
+            if maxReach >= len(nums)-1:
+                return True
+            
+        return True
+
+        
